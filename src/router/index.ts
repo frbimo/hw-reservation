bc4c87e8ab75e632bc62698ba62d34a95f0cb6d6
Bimo Fransiscus Asisi <d11002806@gapps.ntust.edu.tw>
2025-04-11 22:16:13 +0800

    change router config on createWebHashHistory to optimize for gh-pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import LoginView from '../views/LoginView.vue';
 import AppScheduler from '../components/AppScheduler.vue';
 
@@ -9,7 +9,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
